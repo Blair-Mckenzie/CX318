8ce2dcda363f99fc44804590a2f26cdc4fc88eb6
Natalia Magdalena Dombrowska CES2017 <natalia.dombrowska.2017@uni.strath.ac.uk>
2020-02-19 20:49:34 +0000

    get charasteric of service

--- App/src/bluetooth.js
+++ App/src/bluetooth.js
@@ -9,7 +9,22 @@ button.addEventListener("click",() => {
             console.log(device.name);
             return device.gatt.connect();
         })
-        .then(server => { /* ... */ })
+        .then(server => {
+            console.log('got server');
+            return server.getPrimaryService('battery_service');
+         })
+         .then(service => {
+             console.log('got service');
+             return service.getCharacteristic('battery_level');
+         })
+         .then(characteristic => { 
+             console.log('got characteristic');
+             // characteristic.writeValue...
+            return characteristic.readValue();
+         })
+         .then(value => {
+             console.log('battery level', value);
+         })
         .catch(error => {console.log(error);});
 });
 
