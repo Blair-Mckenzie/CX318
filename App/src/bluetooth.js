729d7e6ba4e9d70c2a372712feb1d35c4890484f
Blair McKenzie CES2017 <blair.mckenzie.2017@uni.strath.ac.uk>
2020-03-02 11:15:30 +0000

    Now working with continous button press and release

--- App/src/bluetooth.js
+++ App/src/bluetooth.js
@@ -53,24 +53,3 @@ function sendValue1(value) {
     customChar2.writeValue(buffer);
 }
 
-
-//         .then(device => device.gatt.connect())
-//         .then(server => {
-//             // Getting Battery Service...
-//             return server.getPrimaryService('battery_service');
-//         })
-//         .then(service => {
-//             // Getting Battery Level Characteristic...
-//             return service.getCharacteristic('battery_level');
-//         })
-//         .then(characteristic => {
-//             // Reading Battery Level...
-//             return characteristic.readValue();
-//         })
-//         .then(value => {
-//             console.log('Battery percentage is ' + value.getUint8(0));
-//         })
-//         .catch(error => {
-//             console.log(error);
-//         });
-// });
